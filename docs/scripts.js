196358f48445189fee77b74163d6a41feff52a99
MathieuLd <mlindenpro@gmail.com>
2021-01-17 22:10:40 +0100

    Basic code structure, basic generator root quality, next thing to do add url parser

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,13 +1,201 @@
-function onBodyLoad(){
-	const queryString = window.location.search;
-	console.log(queryString);
-	const urlParams = new URLSearchParams(queryString);
-	console.log(urlParams);
-	for(var value of urlParams.values()) {
-		console.log(value);
+var paramValues = {
+	number:10,
+	root:[0,1,2,3,4,5,6,7,8,9,10,11],
+	quality:[0,1,2,3]
+}
+
+var domElements = {
+	number:"",
+	root:[],
+	quality:[]
+}
+
+function Chord() {
+	this.root = 0;
+	this.quality = 0;
+	this.name = "";
+}
+
+function randomInt(val){
+	return Math.floor(Math.random() * val);
+}
+
+
+Chord.prototype.createName = function () {
+	this.name = "";
+	switch(this.root){
+		case 0:
+			this.name += "C";
+			break;
+		case 1:
+			if (randomInt(2) == 0){
+				this.name += "C#";
+			}else{
+				this.name += "Db";
+			}
+			break;
+		case 2:
+			this.name += "D";
+			break;
+		case 3:
+			if (randomInt(2) == 0){
+				this.name += "D#";
+			}else{
+				this.name += "Eb";
+			}
+			break;
+		case 4:
+			this.name += "E";
+			break;
+		case 5:
+			this.name += "F";
+			break;
+		case 6:
+			if (randomInt(2) == 0){
+				this.name += "F#";
+			}else{
+				this.name += "Gb";
+			}
+			break;
+		case 7:
+			this.name += "G";
+			break;
+		case 8:
+			if (randomInt(2) == 0){
+				this.name += "G#";
+			}else{
+				this.name += "Ab";
+			}
+			break;
+		case 9:
+			this.name += "A";
+			break;
+		case 10:
+			if (randomInt(2) == 0){
+				this.name += "A#";
+			}else{
+				this.name += "Bb";
+			}
+			break;
+		case 11:
+			this.name += "B";
+			break;			
+	}
+	switch(this.quality){
+		case 0:
+			this.name += " maj7";
+			break;
+		case 1:
+			this.name += " min7";
+			break;
+		case 2:
+			this.name += " dom7";
+			break;
+		case 3:
+			this.name += " dim7";
+			break;
 	}
 }
 
-function generateRandomChords() {
-	alert("Hello! I am an alert box!");
+var randomChords = [];
+
+function fillDomElements(){
+	domElements.number = document.getElementById("chordNbr");
+	var rootCheckboxs = Array.prototype.slice.call(document.getElementById("root").getElementsByTagName("input"));
+	for (var i=0; i<rootCheckboxs.length; i++){
+		domElements.root.push(rootCheckboxs[i]);
+	}
+	var qualityCheckboxs = Array.prototype.slice.call(document.getElementById("quality").getElementsByTagName("input"));
+	for (var i=0; i<qualityCheckboxs.length; i++){
+		domElements.quality.push(qualityCheckboxs[i]);
+	}
+}
+
+function clearDomElementsValues(){
+	domElements.number.value = "";
+	for (var i=0; i<domElements.root.length; i++){
+		domElements.root[i].checked = false;
+	}	
+	for (var i=0; i<domElements.quality.length; i++){
+		domElements.quality[i].checked = false;
+	}
+}
+
+function loadParamValues2DomElementsValues(){
+	clearDomElementsValues();
+	
+	domElements.number.value = paramValues.number;
+	for (var i=0; i<paramValues.root.length; i++){
+		domElements.root[paramValues.root[i]].checked = true;
+	}
+	for (var i=0; i<paramValues.quality.length; i++){
+		domElements.quality[paramValues.quality[i]].checked = true;
+	}
+}
+
+function loadUrlParams(urlParms){
+	for(var pair of searchParams.entries()) {
+		console.log(pair[0]+ ', '+ pair[1]);
+	}
+}
+
+function printRandomChords(){
+	console.log(randomChords);
+	for(var i=0; i<randomChords.length; i++){
+		var randomChord = randomChords[i];
+		console.log(randomChord.name);
+	}
+}
+
+
+function generateRandomChords() {	
+	randomChords = [];
+	for(var i=0; i<paramValues.number; i++){
+		var chord = new Chord();
+		chord.root = paramValues.root[randomInt(paramValues.root.length)];
+		chord.quality = paramValues.quality[randomInt(paramValues.quality.length)];
+		chord.createName();
+		randomChords.push(chord);
+	}
+	printRandomChords();
+}
+
+function domElementsValues2ParamValues(){
+	paramValues.number = domElements.number.value;
+	paramValues.root = [];
+	for (var i=0; i<domElements.root.length; i++){
+		if (domElements.root[i].checked){
+			paramValues.root.push(i);
+		}
+	}
+	paramValues.quality = []
+	for (var i=0; i<domElements.quality.length; i++){
+		if (domElements.quality[i].checked){
+			paramValues.quality.push(i);
+		}
+	}
+}
+
+function generateRandomChordsButtonTrigger() {
+	domElementsValues2ParamValues();
+	generateRandomChords();
+}
+
+function loadDefaultParams(){
+	loadParamValues2DomElementsValues();
+	console.log('ok');
+}
+
+
+
+function onBodyLoad(){
+	fillDomElements();
+	console.log(domElements);console.log(paramValues);
+	const queryString = window.location.search;
+	if (queryString === ""){
+		loadDefaultParams();
+	}else{
+		const urlParams = new URLSearchParams(queryString);
+		loadUrlParams(urlParams);
+	}
 }
