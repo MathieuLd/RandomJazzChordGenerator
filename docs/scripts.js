5588399407904fb51ee52b48faf16b814bbd61a9
MathieuLd <mlindenpro@gmail.com>
2021-01-17 18:20:57 +0100

    Test js hosted url v3

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,7 +1,7 @@
 function onBodyLoad(){
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
-	const param = urlParams.getAll()
+	const param = URLSearchParams.values()
 	console.log(queryString);
 	console.log(param);
 }
