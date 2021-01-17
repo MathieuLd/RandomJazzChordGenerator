c93af651c6bf628316ffd00954169a321fc3a7a0
MathieuLd <mlindenpro@gmail.com>
2021-01-17 17:20:56 +0100

    Test js getting url when hosted

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,3 +1,11 @@
-function myFunction() {
-  alert("Hello! I am an alert box!");
+function onBodyLoad(){
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	const num = urlParams.get('num')
+	console.log(queryString);
+	console.log(num);
+}
+
+function generateRandomChords() {
+	alert("Hello! I am an alert box!");
 }
